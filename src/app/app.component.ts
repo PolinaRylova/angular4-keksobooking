2c2975340e71bcfede9ab4c7c9596df2b3ff5265
Polina Rylova <mail@polinarya.ru>
2017-10-04 23:18:42 +0500

    Изменила заголовок и добавила подзаголовок в главный компонент

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Keksobooking';
+  title = 'KEKSOBOOKING';
+  subtitle = 'Кексы по соседству';
 }
