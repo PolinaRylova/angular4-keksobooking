aed4a97de544f1feb4e034b7366e313041bd8f4d
Polina Rylova <mail@polinarya.ru>
2017-10-06 10:12:48 +0500

    Создала компоненты tokyo и notice, исправила ошибку с подключением стилевого файла в head index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { TokyoComponent } from './tokyo/tokyo.component';
+import { NoticeComponent } from './notice/notice.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    TokyoComponent,
+    NoticeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/tokyo/tokyo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tokyo',
+  templateUrl: './tokyo.component.html',
+  styleUrls: ['./tokyo.component.css']
+})
+export class TokyoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
