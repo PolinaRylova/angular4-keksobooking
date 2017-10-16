2f9f8376f103a874744d46256782ed699abb07d2
Polina Rylova <mail@polinarya.ru>
2017-10-16 19:12:54 +0500

    Добавила сервис объявлений

---
+++ src/app/notice/notice.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { Notice } from './notice';
+
+@Injectable()
+export class NoticeService {
+  private noticesUrl = 'api/notices';
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+  constructor(private http: Http) {}
+
+  getNotices(): Promise<Notice[]> {
+    return this.http.get(this.noticesUrl)
+               .toPromise()
+               .then(response => {
+                 return response.json() as Notice[];})
+               .catch(this.handleError);  
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
+
+}
