fff4b17758bc4c6321c7c0054a743409ab588c2e
Polina Rylova <mail@polinarya.ru>
2017-11-10 18:44:11 +0500

    Добавила placeholder с подсказкой допустимых координат в поле адреса и обработчик события input (пока не настроила)

--- src/app/app-notice-form-address/app-notice-form-address.component.ts
+++ src/app/app-notice-form-address/app-notice-form-address.component.ts
@@ -13,4 +13,25 @@ export class AppNoticeFormAddressComponent {
 
   constructor() { }
 
+  inputHandler(event) {
+    const inputStringArr = event.target.value.replace(',', '').split(' ');
+    // Вычленяем элементы массива с координатами x и y и приводим к числу
+    const inputedCoordX = Number(inputStringArr[1]);
+    const inputedCoordY = Number(inputStringArr[3]);
+    // Проверяем, чтобы вводимые значения не выходили за пределы карты
+    if (inputedCoordX <= 1200 && inputedCoordY <= 660) {
+      // Перемещаем метку в соответствии с введенными координатами и размером метки
+      // pinMain.style.left = (inputedCoordX - pinMainWidth / 2) + 'px';
+      // pinMain.style.top = (inputedCoordY - pinMainHeight) + 'px';
+      console.log('Введены допустимые значения');
+    } else {
+      // В случае выхода вводимых значений за диапазон возможных
+      // возвращаем метку и значение в поле по умолчанию
+      // pinMain.style.left = (defaultPinMainCoords.x - pinMainWidth / 2) + 'px';
+      // pinMain.style.top = (defaultPinMainCoords.y - pinMainHeight) + 'px';
+      // addressField.value = 'x: ' + defaultPinMainCoords.x + ', y: ' + defaultPinMainCoords.y;
+      console.log('Введены недопустимые значения');
+    }
+  }
+
 }
