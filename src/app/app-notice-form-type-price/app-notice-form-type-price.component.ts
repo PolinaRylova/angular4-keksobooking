6510a509251f26d7a1dd832451f5652e44581d7e
Polina Rylova <mail@polinarya.ru>
2017-11-16 21:34:13 +0500

    Реализовала привязку полей типа жилья и цены

--- src/app/app-notice-form-type-price/app-notice-form-type-price.component.ts
+++ src/app/app-notice-form-type-price/app-notice-form-type-price.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-notice-form-type-price',
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppNoticeFormTypePriceComponent implements OnInit {
 
+  @ViewChild('typeSelect') typeSelect: ElementRef;
+  selectedIndex: number;
+  minPrice: number;
+  maxPrice = 1000000;
+
   constructor() { }
 
   ngOnInit() {
+    this.selectedIndex = this.typeSelect.nativeElement.selectedIndex;
+    this.switchMinPrice(this.selectedIndex);
   }
 
+  switchMinPrice(selectedIndex) {
+    switch (selectedIndex) {
+      case 0:
+        this.minPrice = 1000;
+        break;
+      case 1:
+        this.minPrice = 0;
+        break;
+      case 2:
+        this.minPrice = 10000;
+        break;
+      case 3:
+        this.minPrice = 50000;
+        break;
+    }
+  }
+
+  saveSelectedIndex(selectedValue) {
+    this.selectedIndex = selectedValue;
+    this.switchMinPrice(this.selectedIndex);
+  }
 }
