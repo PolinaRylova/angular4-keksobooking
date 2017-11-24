5009f8969549fe9f12c2a55aad524b60a2d70df1
Polina Rylova <mail@polinarya.ru>
2017-11-24 19:12:18 +0500

    Добавила привязку ngModel для поля title в форме

--- src/app/app-notice-form-title/app-notice-form-title.component.ts
+++ src/app/app-notice-form-title/app-notice-form-title.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-notice-form-title',
   templateUrl: './app-notice-form-title.component.html',
   styleUrls: ['./app-notice-form-title.component.css']
 })
-export class AppNoticeFormTitleComponent implements OnInit {
+export class AppNoticeFormTitleComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  title: string;
+  constructor() {
   }
 
+  changeTitleValue(value) {
+    console.log(value);
+  }
 }
